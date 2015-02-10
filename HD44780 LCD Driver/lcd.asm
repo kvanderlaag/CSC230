463859896881c48289b3a08934861d76b9fcdad0
kvanderlaag <kvanderlaag@gmail.com>
2015-02-09 17:58:25 -0800

    Working!
    Still needs minor updates to gotoxy, putchar, and delay subroutines

--- HD44780 LCD Driver/lcd.asm
+++ HD44780 LCD Driver/lcd.asm
@@ -1,36 +1,9 @@
-.include "m2560def.inc"
-
-
-#define FCPU			16e6		; MCU clock speed in Hz (1e6 = 1MHz)
-#define LCD_DAT			50		; Execute time in microseconds for data commands
-#define LCD_ENA			1		; Execute time in microseconds for clock pulse
-#define LCD_CLEAR		2		; Execute time for longer commands in ms
-#define LCD_2X16				; LCD size in Rows x Columns.
-#define SPBITS			22		; Stack pointer width in bits for return addresses.
-						; Mega2560 = 22, some others = 16
-#if SPBITS = 22
-#define SPBITS_22
-#endif
-
-
-; Register definitions used in this module
-.def CREG=R18					; Command or data register used in routines
-.def DREG=R19					; Delay values passed to dly_ms and dly_us
-.def TEMP=R16					; Mnemonics for temporary values
-.def TEMP2=R17
-.def RET1=R7 		; These three registers can be used to store
-.def RET2=R8 		; return addresses for working with the stack
-#ifdef SPBITS_22	; If your processor has a 22-bit stack pointer, define
-.def RET3=R9		; a third register byte
-#endif
-
-
 ; ***
 ; HD44780 LCD Driver for ATmega2560.
 ; (DFRobot LCD Keypad Shield v1.1, Arduino Mega2560)
 ;
 ; Author: Keegan van der Laag (jkvander@uvic.ca)
-; Updated 4 February 2015
+; Updated 8 February 2015
 ;
 ; ---
 ;
@@ -38,132 +11,59 @@
 ; provided by Jason Corless (jcorless@uvic.ca).
 ; Delay loops hackishly paraphrased from Atmel's AVR C libraries.
 ;
-; TODO:
-; - Make gotoxy subroutine more robust. Add cases for locations not displayed
-;   on the 16x2 LCD
-; - Implement generics and conditional assembly for other pin assignments
-;   and other LCD sizes.
-
 ; End Notes
 ; ***
 
-; Definitions of constants for different LCD sizes.
-; Shamelessly borrowed from the C driver mentioned above.
-#ifdef LCD_1X8
-#define LCD_COLUMN      8
-#define LCD_LINE        1
-#define LCD_LINE1       0x80
-#endif
+.include "m2560def.inc"
 
-#ifdef LCD_1X16
-#define LCD_COLUMN      16
-#define LCD_LINE        1
-#define LCD_LINE1       0x80
-#endif
 
-#ifdef LCD_1X20
-#define LCD_COLUMN      20
-#define LCD_LINE        1
-#define LCD_LINE1       0x80
-#endif
+; Define the LCD size in rows x columns. Constants are conditionally
+; determined based on this, and should be compatible with any HD44780
+; controlled display.
+#define LCD_2X16
 
-#ifdef LCD_1X40
-#define LCD_COLUMN      40
-#define LCD_LINE        1
-#define LCD_LINE1       0x80
-#endif
 
-#ifdef LCD_2X8
-#define LCD_COLUMN      8
-#define LCD_LINE        2
-#define LCD_LINE1       0x80
-#define LCD_LINE2       (0x80 + 0x40)
-#endif
 
-#ifdef LCD_2X12
-#define LCD_COLUMN      12
-#define LCD_LINE        2
-#define LCD_LINE1       0x80
-#define LCD_LINE2       (0x80 + 0x40)
-#endif
 
-#ifdef LCD_2X16
-#define LCD_COLUMN      16
-#define LCD_LINE        2
-#define LCD_LINE1       0x80
-#define LCD_LINE2       (0x80 + 0x40)
-#endif
 
-#ifdef LCD_2X20
-#define LCD_COLUMN      20
-#define LCD_LINE        2
-#define LCD_LINE1       0x80
-#define LCD_LINE2       (0x80 + 0x40)
-#endif
-
-#ifdef LCD_2X24
-#define LCD_COLUMN      24
-#define LCD_LINE        2
-#define LCD_LINE1       0x80
-#define LCD_LINE2       (0x80 + 0x40)
-#endif
-
-#ifdef LCD_2X40
-#define LCD_COLUMN      40
-#define LCD_LINE        2
-#define LCD_LINE1       0x80
-#define LCD_LINE2       (0x80 + 0x40)
-#endif
-
-#ifdef LCD_4X16
-#define LCD_COLUMN      16
-#define LCD_LINE        4
-#define LCD_LINE1       0x80
-#define LCD_LINE2       (0x80 + 0x40)
-#define LCD_LINE3       (0x80 + 0x10)
-#define LCD_LINE4       (0x80 + 0x50)
-#endif
-
-#ifdef  LCD_4X20
-#define LCD_COLUMN      20
-#define LCD_LINE        4
-#define LCD_LINE1       0x80
-#define LCD_LINE2       (0x80 + 0x40)
-#define LCD_LINE3       (0x80 + 0x14)
-#define LCD_LINE4       (0x80 + 0x54)
-#endif
-
-; End of LCD Constant Definitions
-
-; Some constant definitions for Hitachi HD44780 Command instructions.
-; Can be used with lcd_cmd by pushing them onto the stack before calling.
-#define cmd_CLR			0x01
-#define cmd_HOM			0x02
-
-; End of LCD Constant Definitions
-; ***
 
 ; ***
 ; LCD Pin Definitions.
 ; Changing these should affect lcd_init, lcd_nbl, lcd_byte, and lcd_putchar
 
 ; Data Pins B4-B7
-#define P_4 = G
-#define DB4 = (1<<PG5)
-#define P_5 = E
-#define DB5 = (1<<PE3)
-#define P_6 = H
-#define DB6 = (1<<PH3)
-#define P_7 = H
-#define DB7 = (1<<PH4)
 
-; Command Pins
-; E (Clock) = D9 = H6
-#define P_ENA = H
-#define ENABLE = (1<<PH6)
-; RS (Cmd/Data) = D8 = H5
-#define P_RS = H
-#define REGSEL = (1<<PH5)
+.equ	LCD_PORT_D4  =  PORTG
+.equ	LCD_PORT_D5  =	PORTE
+.equ	LCD_PORT_D6  =	PORTH
+.equ	LCD_PORT_D7  =	PORTH
+	
+.equ	PIN_D4	  =		5
+.equ	PIN_D5	  =		3
+.equ	PIN_D6	  =		3
+.equ	PIN_D7	  =		4
+
+.equ	LCD_PORT_ENA =	PORTH
+.equ 	LCD_PORT_RS  =	PORTH
+
+.equ	PIN_ENA	  =		6
+.equ	PIN_RS	  =		5
+
+; Include LCD Driver Constants and Conditionals
+.include "LCDdefs.inc"
+
+; ***
+; End of LCD Pin Definitions
+
+
+
+; End of LCD Constant Definitions
+; ***
+
+;Inline
+
+; End Inline
+
 
 ; End of Pin Definitions
 ; ***
@@ -245,41 +145,43 @@ subroutinedefinitions: jmp subroutinedefinitions 	; Just in case.
 lcd_nbl:
 	; CREG(7, 6) -> PORTH(4, 3)
 
-	lds TEMP, PINH
-	cbr TEMP, (1<<PH3)+(1<<PH4)
-	sbrc CREG, 7
-	sbr TEMP, (1<<PH4)
-	sbrc CREG, 6
-	sbr TEMP, (1<<PH3)
-	sts PORTH, TEMP
-
-	; CREG(5) -> PORTE(3)
-	lds TEMP, PINE+0x20
-	cbr TEMP, (1<<PE3)
-	sbrc CREG, 5
-	sbr TEMP, (1<<PE3)
-	sts PORTE+0x20, TEMP
-
-	; CREG(4) -> PORTG(5)
-	lds TEMP, PING+0x20
-	cbr TEMP, (1<<PG5)
+	lds TEMP, PINS_D4
+	cbr TEMP, (1<<PIN_D4)
 	sbrc CREG, 4
-	sbr TEMP, (1<<PG5)
-	sts PORTG+0x20, TEMP
+	sbr TEMP, (1<<PIN_D4)
+	sts PORT_D4, TEMP
+
+	lds TEMP, PINS_D5
+	cbr TEMP, (1<<PIN_D5)
+	sbrc CREG, 5
+	sbr TEMP, (1<<PIN_D5)
+	sts PORT_D5, TEMP
+
+	lds TEMP, PINS_D6
+	cbr TEMP, (1<<PIN_D6)
+	sbrc CREG, 6
+	sbr TEMP, (1<<PIN_D6)
+	sts PORT_D6, TEMP
+
+	lds TEMP, PINS_D7
+	cbr TEMP, (1<<PIN_D7)
+	sbrc CREG, 7
+	sbr TEMP, (1<<PIN_D7)
+	sts PORT_D7, TEMP
 
 	; Pulse clock high
-	lds TEMP, PINH
-	sbr TEMP, (1<<PH6)
-	sts PORTH, TEMP 
+	lds TEMP, PINS_ENA
+	sbr TEMP, (1<<PIN_ENA)
+	sts PORT_ENA, TEMP 
 	
 	; Wait for LCD_ENA microseconds
 	ldi DREG, 0x05
 	call dly_us
 	
 	; Pulse clock low.
-	lds TEMP, PINH
-	cbr TEMP,  (1<<PH6)
-	sts PORTH, TEMP
+	lds TEMP, PINS_ENA
+	cbr TEMP,  (1<<PIN_ENA)
+	sts PORT_ENA, TEMP
  
 	; Return
 	ret
@@ -297,13 +199,13 @@ lcd_byte:
 	; Get stack data into CREG
 	pop RET1
 	pop RET2
-	#ifdef SPBITS_22
+	.if SPBITS > 16
 	pop RET3
-	#endif
+	.endif
 	pop CREG
-	#ifdef SPBITS_22
+	.if SPBITS > 16
 	push RET3
-	#endif
+	.endif
 	push RET2
 	push RET1
 	; Send high nibble
@@ -318,17 +220,17 @@ lcd_byte:
 	ldi DREG, LCD_DAT
 	call dly_us
 	swap CREG
-	#ifdef SPBITS_22
+	.if SPBITS > 16
 	pop RET3
-	#endif
+	.endif
 	pop RET2
 	pop RET1
 	push CREG
 	push RET1
 	push RET2
-	#ifdef SPBITS_22
+	.if SPBITS > 16
 	push RET3
-	#endif
+	.endif
 	ret
 ; **
 
@@ -343,20 +245,20 @@ lcd_byte:
 lcd_cmd:
 	pop RET1
 	pop RET2
-	#ifdef SPBITS_22
+	.if SPBITS > 16
 	pop RET3
-	#endif
+	.endif
 	pop CREG
-	#ifdef SPBITS_22
+	.if SPBITS > 16
 	push RET3
-	#endif
+	.endif
 	push RET2
 	push RET1
 
 	; Set RS = 0
-	lds TEMP, PINH
-	cbr TEMP, (1<<PH5)
-	sts PORTH, TEMP
+	lds TEMP, PINS_RS
+	cbr TEMP, (1<<PIN_RS)
+	sts PORT_RS, TEMP
 	; Send commnand byte (dat)
 	push CREG
 	call lcd_byte
@@ -384,19 +286,19 @@ cmd_fin:
 lcd_putchar:
 	pop RET1
 	pop RET2
-	#ifdef SPBITS_22
+	.if SPBITS > 16
 	pop RET3
-	#endif
+	.endif
 	pop CREG
-	#ifdef SPBITS_22
+	.if SPBITS > 16
 	push RET3
-	#endif
+	.endif
 	push RET2
 	push RET1
 	; Set RS = 1 (Write data to current DDRAM address)
-	lds TEMP, PINH
-	sbr TEMP, (1<<PH5)
-	sts PORTH, TEMP
+	lds TEMP, PINS_RS
+	sbr TEMP, (1<<PIN_RS)
+	sts PORT_RS, TEMP
 	; Send character data in byte (dat) using lcd_byte
 	push CREG
 	call lcd_byte
@@ -461,13 +363,13 @@ lcd_puts:		; Load two-byte address in str into X register
 lcd_gotoxy:
 	pop RET1
 	pop RET2
-	#ifdef SPBITS_22
+	.if SPBITS > 16
 	pop RET3
-	#endif
+	.endif
 	pop TEMP
-	#ifdef SPBITS_22
+	.if SPBITS > 16
 	push RET3
-	#endif
+	.endif
 	push RET2
 	push RET1
 	
@@ -530,26 +432,47 @@ lcd_init:
 	; Set Data Direction Register bits to output for LCD data 4-7,
 	; E, and RS.
 	; TODO: Abstract this for different pin assignments.
-	lds TEMP, DDRH
-	sbr TEMP, (1<<PH3)+(1<<PH4)+(1<<PH5)+(1<<PH6)
-	sts DDRH, TEMP
-	lds TEMP, PINH
-	cbr TEMP, (1<<PH3)+(1<<PH4)+(1<<PH5)+(1<<PH6)
-	sts PORTH, TEMP
-	
-	lds TEMP, DDRE+0x20
-	sbr TEMP, (1<<PE3)
-	sts DDRE+0x20, TEMP
-	lds TEMP, PINE+0x20
-	cbr TEMP, (1<<PE3)
-	sts PORTE+0x20, TEMP
+	lds TEMP, DDR_D4
+	sbr TEMP, (1<<PIN_D4)
+	sts DDR_D4, TEMP
+	lds TEMP, PINS_D4
+	cbr TEMP, (1<<PIN_D4)
+	sts PORT_D4, TEMP
 
-	lds TEMP, DDRG+0x20
-	sbr TEMP, (1<<PG5)
-	sts DDRG+0x20, TEMP
-	lds TEMP, PING+0x20
-	cbr TEMP, (1<<PG5)
-	sts PORTG+0x20, TEMP
+	lds TEMP, DDR_D5
+	sbr TEMP, (1<<PIN_D5)
+	sts DDR_D5, TEMP
+	lds TEMP, PINS_D5
+	cbr TEMP, (1<<PIN_D5)
+	sts PORT_D5, TEMP
+
+	lds TEMP, DDR_D6
+	sbr TEMP, (1<<PIN_D6)
+	sts DDR_D6, TEMP
+	lds TEMP, PINS_D6
+	cbr TEMP, (1<<PIN_D6)
+	sts PORT_D6, TEMP
+
+	lds TEMP, DDR_D7
+	sbr TEMP, (1<<PIN_D7)
+	sts DDR_D7, TEMP
+	lds TEMP, PINS_D7
+	cbr TEMP, (1<<PIN_D7)
+	sts PORT_D7, TEMP
+	
+	lds TEMP, DDR_RS
+	sbr TEMP, (1<<PIN_RS)
+	sts DDR_RS, TEMP
+	lds TEMP, PINS_RS
+	cbr TEMP, (1<<PIN_RS)
+	sts PORT_RS, TEMP
+
+	lds TEMP, DDR_ENA
+	sbr TEMP, (1<<PIN_ENA)
+	sts DDR_ENA, TEMP
+	lds TEMP, PINS_ENA
+	cbr TEMP, (1<<PIN_ENA)
+	sts PORT_ENA, TEMP
 
 	; Initialize display to specs listed in HD44780 data sheet.
 	; Generally very conservative with timing; speed may be improved
@@ -591,7 +514,7 @@ dly_init:
 	ldi TEMP, 0x06		; Increment cursor, no Display Shift
 	push TEMP
 	call lcd_cmd
-	ldi TEMP, 0x0F		; Display On, Cursor On, Blink On
+	ldi TEMP, 0x0C		; Display On, Cursor On, Blink On
 	push TEMP
 	call lcd_cmd
 	ldi TEMP, 0x00
@@ -636,7 +559,7 @@ dly_ms:
 		; Y =  DREG*(4000), 4000 = 2048 + 1024 + 512 + 256 + 128 + 64 + 16
 		;						4000 =		    1111 1101 0000
 		;                               DREG     F    D    0
-		; 0xFD * DREG * 16
+		; 0xFD * DREG * FCPU/1e6
 		; DREG is 0x00 to 0xF0, low nibble is always ignored
 		; This means that delays up to about 15ms can be achieved using
 		; A 16-bit register pair. (Y-register)
@@ -667,9 +590,9 @@ dlyms:	sbiw YH:YL, 1
 str_init:
 	pop RET1
 	pop RET2
-	#ifdef SPBITS_22
+	.if SPBITS > 16
 	pop RET3
-	#endif
+	.endif
 
 	pop ZL
 	pop ZH
@@ -677,9 +600,9 @@ str_init:
 	pop XL
 	pop XH
 
-	#ifdef SPBITS_22
+	.if SPBITS > 16
 	push RET3
-	#endif
+	.endif
 	push RET2
 	push RET1
 
@@ -702,11 +625,8 @@ initdone:
 
 
 ; ***
-; Initialization values for String
-init:	.db		0x48, 0x65, 0x6C, 0x6C, 0x6F, 0x2C, 0xA0, 0x57 
-init2:	.db		0x6F, 0x72, 0x6C, 0x64, 0x21, 0x00, 0x00, 0x00
-init3:	.db		0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
-init4:	.db		0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+; Initialization values ro4for String
+init:	.db		"Hello, World!", '\0'
 
 
 
@@ -714,10 +634,8 @@ init4:	.db		0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
 ; Memory Allocation
 
 .dseg
-	str: .byte 33 ; Reserve 33 characters worth of memory (16x2 character LCD),
-				  ; lcd_puts traverses memory until it finds a 0x00 byte. For
-				  ; the love of god, make sure that (str+0x20) = 0x00 when you
-				  ; set it.
+
+	str: .byte lcd_length ;
 
 	cursor_xy:	.byte 1 ; Reserve one byte for the current cursor position.
 						; cursor_xy = rrrrcccc, where R is row (0x00 or (0x01)
